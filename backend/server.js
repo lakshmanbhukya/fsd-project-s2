549daf1377c5b11c445fc49a53c15707217e1477
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-11-07 18:31:13 +0530

    feat : deployment updates

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,14 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? ["https://product-review-1119.netlify.app/"]
-        : ["http://localhost:3000"],
+    origin: [
+      "https://product-review-1119.netlify.app",
+      "http://localhost:3000",
+      "http://localhost:3001"
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 app.use(express.json());
